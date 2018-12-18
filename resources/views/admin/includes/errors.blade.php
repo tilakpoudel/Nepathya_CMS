1a058ec39cb25358e2d6dac2aef1c5603634d6a9
tilak <tilak.poudel.314@gmail.com>
2018-12-18 23:33:43 +0545

    commit after overview

--- resources/views/admin/includes/errors.blade.php
+++ resources/views/admin/includes/errors.blade.php
@@ -1,4 +1,4 @@
-@if(count($errors)>0)
+@if(count($errors) > 0)
   <ul class="list-group">
         @foreach($errors->all() as $error)
           <li class=" list-group-item text-danger">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,13 +7,16 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'NepathyaCms') }}</title>
 
     <!-- Scripts -->
     
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+    {{-- <script src="{{asset('bootstrap/css/bootstrap.min.css')}}"></script>
+    <script src="{{asset('bootstrap/css/bootstrap.min.js')}}"></script> --}}
+
     <script src="{{ asset('js/app.js') }}" defer></script>
     <link rel="stylesheet" href="{{asset('css/toastr.min.css')}}">
 
